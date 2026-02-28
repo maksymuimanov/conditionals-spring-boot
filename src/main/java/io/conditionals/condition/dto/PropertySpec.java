d006eed190964f585d822d48e54818312ffa7459
Maksym Uimanov <uimanovmaks@gmail.com>
2026-02-28 01:10:30 +0100

    Refactor `PropertySpec`: rename utility methods for clarity, add encapsulation with new getters, and improve error handling with `ConversionException` handling.

--- src/main/java/io/conditionals/condition/dto/PropertySpec.java
+++ src/main/java/io/conditionals/condition/dto/PropertySpec.java
@@ -2,6 +2,7 @@ package io.conditionals.condition.dto;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.convert.ConversionException;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
@@ -27,16 +28,15 @@ public class PropertySpec<V, S extends PropertySpec<V, S>> {
     protected PropertySpec(Class<? extends Annotation> annotationType,
                            AnnotationAttributes annotationAttributes) {
         this.annotationType = annotationType;
-        this.prefix = !annotationAttributes.containsKey(PREFIX)
-                ? ""
-                : this.getPrefix(annotationAttributes);
-        this.names = this.getNames(annotationAttributes);
+        this.prefix = this.resolvePrefix(annotationAttributes);
+        this.names = this.resolveNames(annotationAttributes);
         this.havingValue = (V) annotationAttributes.get(HAVING_VALUE);
         this.havingValueType = (Class<V>) this.havingValue.getClass();
         this.matchIfMissing = annotationAttributes.getBoolean(MATCH_IF_MISSING);
     }
 
-    protected String getPrefix(AnnotationAttributes annotationAttributes) {
+    protected String resolvePrefix(AnnotationAttributes annotationAttributes) {
+        if (!annotationAttributes.containsKey(PREFIX)) return "";
         String prefix = annotationAttributes.getString(PREFIX).trim();
         if (StringUtils.hasText(prefix) && !prefix.endsWith(".")) {
             prefix = prefix + ".";
@@ -45,13 +45,13 @@ public class PropertySpec<V, S extends PropertySpec<V, S>> {
         return prefix;
     }
 
-    protected String[] getNames(AnnotationAttributes annotationAttributes) {
+    protected String[] resolveNames(AnnotationAttributes annotationAttributes) {
         String[] value = (String[]) annotationAttributes.get(VALUE);
         String[] name = (String[]) annotationAttributes.get(NAME);
         Assert.state(value.length > 0 || name.length > 0,
-                () -> "The name or value attribute of @%s must be specified".formatted(ClassUtils.getShortName(this.annotationType)));
+                () -> "The name or value attribute of @%s must be specified".formatted(ClassUtils.getShortName(this.getAnnotationType())));
         Assert.state(value.length == 0 || name.length == 0,
-                () -> "The name and value attributes of @%s are exclusive".formatted(ClassUtils.getShortName(this.annotationType)));
+                () -> "The name and value attributes of @%s are exclusive".formatted(ClassUtils.getShortName(this.getAnnotationType())));
         return value.length > 0
                 ? value
                 : name;
@@ -61,14 +61,18 @@ public class PropertySpec<V, S extends PropertySpec<V, S>> {
                                   List<String> missing,
                                   List<String> nonMatching,
                                   PropertySpecMatcher<V, S> matcher) {
-        for(String name : this.names) {
-            String key = this.prefix + name;
-            if (resolver.containsProperty(key)) {
-                if (!this.isMatch(resolver.getProperty(key, this.havingValueType), matcher)) {
-                    nonMatching.add(name);
+        for(String name : this.getNames()) {
+            try {
+                String key = this.getPrefix() + name;
+                if (resolver.containsProperty(key)) {
+                    if (!this.isMatch(resolver.getProperty(key, this.getHavingValueType()), matcher)) {
+                        nonMatching.add(name);
+                    }
+                } else if (!this.isMatchIfMissing()) {
+                    missing.add(name);
                 }
-            } else if (!this.matchIfMissing) {
-                missing.add(name);
+            } catch (ConversionException e) {
+                nonMatching.add(name);
             }
         }
 
@@ -76,22 +80,46 @@ public class PropertySpec<V, S extends PropertySpec<V, S>> {
 
     @SuppressWarnings("unchecked")
     protected boolean isMatch(@Nullable V value, PropertySpecMatcher<V, S> matcher) {
-        return matcher.compare((S) this, value, this.havingValue);
+        return matcher.compare((S) this, value, this.getHavingValue());
+    }
+
+    protected Class<? extends Annotation> getAnnotationType() {
+        return annotationType;
+    }
+
+    protected String getPrefix() {
+        return prefix;
+    }
+
+    protected String[] getNames() {
+        return names;
+    }
+
+    protected V getHavingValue() {
+        return havingValue;
+    }
+
+    protected Class<V> getHavingValueType() {
+        return havingValueType;
+    }
+
+    protected boolean isMatchIfMissing() {
+        return matchIfMissing;
     }
 
     public String toString() {
         StringBuilder result = new StringBuilder()
                 .append("(")
-                .append(this.prefix);
-        if (this.names.length == 1) {
-            result.append(this.names[0]);
+                .append(this.getPrefix());
+        if (this.getNames().length == 1) {
+            result.append(this.getNames()[0]);
         } else {
             result.append("[")
-                    .append(StringUtils.arrayToCommaDelimitedString(this.names))
+                    .append(StringUtils.arrayToCommaDelimitedString(this.getNames()))
                     .append("]");
         }
         return result.append("=")
-                .append(this.havingValue)
+                .append(this.getHavingValue())
                 .append(")")
                 .toString();
     }
